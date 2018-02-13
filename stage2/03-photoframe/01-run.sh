9e363faf49c054025aa6651242137b56504bc3f4
Henric Andersson <ha@sensenet.nu>
2018-02-12 17:16:56 -0800

    Updated image name and fixed how we install a readme

--- stage2/03-photoframe/01-run.sh
+++ stage2/03-photoframe/01-run.sh
@@ -4,9 +4,7 @@ on_chroot << EOF
 systemctl disable getty@tty1.service
 EOF
 
-on_chroot << EOF
 install -m 644 files/colortemp_info.txt ${ROOTFS_DIR}/root/colortemp_info.txt
-EOF
 
 on_chroot << EOF
 git clone https://github.com/mrworf/photoframe.git
