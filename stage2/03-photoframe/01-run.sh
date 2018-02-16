c9f0658e3b92bf1145d5aec6a01b91d149f2da79
Henric Andersson <ha@sensenet.nu>
2018-02-16 08:28:57 -0800

    Updated to allow config files from /boot
    Also fixed issue where git clone would fail inside qemu

--- stage2/03-photoframe/01-run.sh
+++ stage2/03-photoframe/01-run.sh
@@ -5,10 +5,19 @@ systemctl disable getty@tty1.service
 EOF
 
 install -m 644 files/colortemp_info.txt ${ROOTFS_DIR}/root/colortemp_info.txt
+install -m 644 files/http-auth.json ${ROOTFS_DIR}/boot/http-auth.json
+
+# Cannot be run with QEMU since it will hang
+git clone -v https://github.com/mrworf/photoframe.git ${ROOTFS_DIR}/root/photoframe
 
 on_chroot << EOF
-git clone https://github.com/mrworf/photoframe.git
-cd photoframe
+echo "done git cloning"
+cd /root/photoframe
+echo "cd"
 cp frame.service /etc/systemd/system/
+echo "copy"
 systemctl enable /etc/systemd/system/frame.service
+echo "systemctl"
+# Enable auto update
+echo >>/etc/crontab "15 3    * * *   root    /root/photoframe/update.sh"
 EOF
