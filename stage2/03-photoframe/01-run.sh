6345890ef7e6853e70075583fcd05696a9fe9d80
Henric Andersson <git@sensenet.nu>
2018-02-22 07:53:12 -0800

    Working photoframe generator

--- stage2/03-photoframe/01-run.sh
+++ stage2/03-photoframe/01-run.sh
@@ -2,10 +2,14 @@
 
 on_chroot << EOF
 systemctl disable getty@tty1.service
+systemctl mask plymouth-start.service
 EOF
 
 install -m 644 files/colortemp_info.txt ${ROOTFS_DIR}/root/colortemp_info.txt
 install -m 644 files/http-auth.json ${ROOTFS_DIR}/boot/http-auth.json
+
+# Remove wait on network
+rm ${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf
 
 # Cannot be run with QEMU since it will hang
 git clone -v https://github.com/mrworf/photoframe.git ${ROOTFS_DIR}/root/photoframe
