db47f9fb2fb5432e80bd4617d3312a2c52bdce8d
Henric Andersson <ha@sensenet.nu>
2018-06-11 09:26:29 -0700

    Add i2c-dev to image

--- stage2/03-photoframe/01-run.sh
+++ stage2/03-photoframe/01-run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -e
 
+# Disable first console and boot messages
 on_chroot << EOF
 systemctl disable getty@tty1.service
 systemctl mask plymouth-start.service
 EOF
 
+# Add info files
 install -m 644 files/colortemp_info.txt ${ROOTFS_DIR}/root/colortemp_info.txt
+
+# Add default authentication
 install -m 644 files/http-auth.json ${ROOTFS_DIR}/boot/http-auth.json
 
 # Remove wait on network
@@ -24,4 +28,7 @@ systemctl enable /etc/systemd/system/frame.service
 echo "systemctl"
 # Enable auto update
 echo >>/etc/crontab "15 3    * * *   root    /root/photoframe/update.sh"
+
+# Add missing i2c-dev modules for color sensor
+echo >>/etc/modules "i2c-dev"
 EOF
