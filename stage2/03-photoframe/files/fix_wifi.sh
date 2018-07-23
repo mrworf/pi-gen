dcc1ca1021ed9245013ae0058cbeea586a8a5826
Henric Andersson <ha@sensenet.nu>
2018-07-23 07:26:03 -0700

    Improved wifi monitoring script

--- stage2/03-photoframe/files/fix_wifi.sh
+++ stage2/03-photoframe/files/fix_wifi.sh
@@ -7,11 +7,18 @@
 # By killing wpa_supplicant and then trying to restart network again,
 # it should rectify this situation.
 #
+# Now also continously monitors wifi and triggers restarts to make sure
+# frame stays connected to wifi.
 
-iwconfig wlan0 | grep 'ESSID:off/any' >/dev/null
-if [ $? -eq 0 ]; then
-	if ! /etc/init.d/networking restart ; then
-		killall wpa_supplicant
-		/etc/init.d/networking restart
+while true ; do
+	if iwconfig wlan0 | grep 'ESSID:off/any' ; then
+		if ! /etc/init.d/networking restart ; then
+			killall wpa_supplicant
+			sleep 2s # Make sure it's gone
+			/etc/init.d/networking restart
+		fi
+		sleep 30s # Give it extra time...
 	fi
-fi
+	sleep 30s
+done
+
